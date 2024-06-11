dace1dd7f0f1f403fe65b12cacf6b376f565c7e9
son Heung-min <c0a2303678@edu.teu.ac.jp>
2024-06-11 17:55:57 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -17,25 +17,30 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         x = tmr%3200
+        x1 = 0
+        y1 = 0
+        kk_rct.move_ip(-1, 0)
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
         key_lst = pg.key.get_pressed()  
         if key_lst[pg.K_UP]:  
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]: 
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:  
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:  
-            kk_rct.move_ip(+1, 0)
+            y1 -= 1
+        elif key_lst[pg.K_DOWN]: 
+            y1 += 1
+        elif key_lst[pg.K_LEFT]:  
+            x1 -= 1
+        elif key_lst[pg.K_RIGHT]:  
+            x1 += 2
         screen.blit(kk_img, kk_rct)  
+        kk_rct.move_ip(x1, y1)
         pg.display.update()
         tmr += 1
         clock.tick(200)
